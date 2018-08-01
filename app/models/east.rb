2082ec8604daabcd7aafa39c806bd60d8948db5d
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2018-08-01 20:24:33 -0300

    Defines East's tests

--- app/models/east.rb
+++ app/models/east.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 class East
+  extend ActiveModel::Naming
+
+  include Compass
+
+  def to_left
+    compass_pointer('NORTH')
+  end
+
+  def to_right
+    compass_pointer('SOUTH')
+  end
+
+  def move(x, y, size_step)
+    Coordinate.new(x: x + size_step, y: y)
+  end
+
+  def report
+    model_name.name.upcase
+  end
 end
