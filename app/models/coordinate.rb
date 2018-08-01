6fe64422f490cfa967d33993781940d354c2e12e
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2018-08-01 19:37:44 -0300

    Coordinate #report return x y value

--- app/models/coordinate.rb
+++ app/models/coordinate.rb
@@ -6,4 +6,8 @@ class Coordinate
   attr_accessor :x, :y
 
   validates :x, :y, presence: true
+
+  def report
+    "#{@x} #{@y}"
+  end
 end
--- spec/models/coordinate_spec.rb
+++ spec/models/coordinate_spec.rb
@@ -4,11 +4,17 @@
 require 'rails_helper'
 
 RSpec.describe Coordinate, type: :model do
-  subject { Coordinate.new(x: 1, y: 1) }
+  subject { Coordinate.new(x: 1, y: 2) }
 
   describe 'validations' do
     %i[x y].each do |field|
       it { is_expected.to validate_presence_of field }
+    end
+  end
+
+  describe '#report' do
+    it 'return x y value' do
+      expect(subject.report).to eq '1 2'
     end
   end
 end
