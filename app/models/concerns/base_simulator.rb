7c4b65bb2e3edd5841f93c5487d7d350fe9155b3
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2018-08-02 02:28:20 -0300

    Defines script simulator by file input

---
+++ app/models/concerns/base_simulator.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module BaseSimulator
+  AXIS_X = 5
+  AXIS_Y = 5
+  UNIT_GRID = 1
+
+  include Compass
+
+  def simulate
+    report = ''
+    default_commands.each do |line|
+      if line.include?('PLACE')
+        @robot = get_robot(line.delete("\n"))
+      elsif @robot
+        report = @robot.play(line.delete("\n").strip)
+      end
+    end
+    report
+  end
+
+  protected
+
+  def get_robot(line)
+    commands = get_commands(line)
+    coordinate = new_coordinate(commands)
+    orientation = compass_pointer(commands.last)
+    if coordinate.valid? && @tabletop.valid?
+      Robot.new(coordinate, orientation, @tabletop)
+    end
+  end
+
+  def get_commands(line)
+    line.split(' ').last.split(',')
+  end
+
+  def new_coordinate(commands)
+    Coordinate.new(x: commands[0].to_i, y: commands[1].to_i)
+  end
+end
--- script/robot_simulator.rb
+++ script/robot_simulator.rb
@@ -1,49 +1,18 @@
 # frozen_string_literal: true
 
 class RobotSimulator
-  AXIS_X = 5
-  AXIS_Y = 5
-  UNIT_GRID = 1
-
-  include Compass
+  include BaseSimulator
 
   def initialize(path_file)
     @path_file = path_file
     @tabletop = Tabletop.new(axisX: AXIS_X, axisY: AXIS_Y, unit_grid: UNIT_GRID)
   end
 
-  def simulate
-    File.readlines(@path_file).each do |line|
-      if line.include?('PLACE')
-        @robot = get_robot(line.delete("\n"))
-      elsif @robot
-        report = @robot.play(line.delete("\n"))
-        print(report)
-      end
-    end
-  end
-
-  private
-
-  def get_robot(line)
-    commands = get_commands(line)
-    coordinate = new_coordinate(commands)
-    orientation = compass_pointer(commands.last)
-    Robot.new(coordinate, orientation, @tabletop) if coordinate.valid? && @tabletop.valid?
-  end
-
-  def get_commands(line)
-    line.split(' ').last.split(',')
-  end
-
-  def new_coordinate(commands)
-    Coordinate.new(x: commands[0].to_i, y: commands[1].to_i)
-  end
-
-  def print(report)
-    p report if report.is_a? String
+  def default_commands
+    File.readlines(@path_file)
   end
 end
 
 simulator = RobotSimulator.new('script/commands.txt')
-simulator.simulate
+report = simulator.simulate
+p report if report.is_a? String
