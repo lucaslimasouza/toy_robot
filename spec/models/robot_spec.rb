2d168d4467d11615668ca100900627f7fd6c0769
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2018-08-01 22:20:22 -0300

    Robot#play facing to SOUTH defines tests

--- spec/models/robot_spec.rb
+++ spec/models/robot_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Robot, type: :model do
   let!(:tabletop) { Tabletop.new(axisX: 5, axisY: 5, unit_grid: 1) }
 
   describe '#play' do
-    context 'facing to North' do
+    context 'facing to NORTH' do
       let!(:orientation) { North.new }
       subject do
         Robot.new(
@@ -32,6 +32,34 @@ RSpec.describe Robot, type: :model do
         subject.play('RIGHT')
 
         expect(subject.report).to eq '1,2,EAST'
+      end
+    end
+
+    context 'facing to SOUTH' do
+      let!(:orientation) { South.new }
+      subject do
+        Robot.new(
+          coordinate,
+          orientation,
+          tabletop
+        )
+      end
+
+      it 'move one step on Tabletop' do
+        subject.play('MOVE')
+        expect(subject.report).to eq '1,1,SOUTH'
+      end
+
+      it 'turn left on Tabletop' do
+        subject.play('LEFT')
+
+        expect(subject.report).to eq '1,2,EAST'
+      end
+
+      it 'turn right on Tabletop' do
+        subject.play('RIGHT')
+
+        expect(subject.report).to eq '1,2,WEST'
       end
     end
   end
